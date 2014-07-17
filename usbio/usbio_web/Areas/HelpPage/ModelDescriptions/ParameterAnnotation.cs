3177151df42cefa6e8e4f960bfd81171679736af
tatsuya kobayashi <tkey.dll@gmail.com>
2014-07-18 00:26:39 +0900

    Add usbio_web project.

---
+++ usbio/usbio_web/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace usbio_web.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
