edc12e61fda3fe1f076705583dd2c9e83e71d53c
tatsuya kobayashi <tkey.dll@gmail.com>
2014-07-18 00:57:45 +0900

    Add UsbioController.

---
+++ usbio/usbio_web/Controllers/UsbioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace usbio_web.Controllers
+{
+    public class UsbioController : ApiController
+    {
+        /// <summary>
+        /// POST api/Usbio/5
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public string Post(int time)
+        {
+            string ret = time.ToString();
+
+            return ret;
+        }
+    }
+}
